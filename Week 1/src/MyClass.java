1f1a2a0c125a6cc6a002beb8fa6378f9658a11a7
Spurtastic <ash.sarith@gmail.com>
2020-08-15 18:32:31 +0800

    the practice so far

---
+++ Week 1/src/MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+
+    public static void main (String[] args){
+        System.out.println("hello world");
+        int x =3, y=4, z=6;
+        System.out.println(x + y +z);
+        int b = Math.max(x,y);
+        System.out.println(b);
+        String random = "sentence is one"; // so the gist is that the index of the first chartacter of the first occurring reference is called upon like i call is, "i" in is occurs at the 9th index as a result
+
+        System.out.println(random.indexOf("is"));
+
+    }
+}

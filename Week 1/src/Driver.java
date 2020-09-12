5ccd5fc68d17c149be2e0d5f544ac2e841d2bad0
Ashwin Sarith <ashwinsarith@192.168.5.206>
2020-09-12 14:09:01 +0800

    some comments

---
+++ Week 1/src/Driver.java
@@ -0,0 +1,11 @@
+
+
+
+// main class----uppercase driver
+public class Driver {
+    // main method----upperCaseHereAndEarlierExceptTheFirst
+    // remember public is accessible by all other classes, the void just means that there is no return statement
+    public static void main(String[] args) {
+        System.out.println("fuck you");
+    }
+}
---
+++ Week 1/src/Structure.md
@@ -0,0 +1,4 @@
+## Java program structures
+## How the execution works.
+    * So essentially the Source code is taken, put through a compiler and then converted to bytecode
+    * 
